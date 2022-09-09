b62faceeb4962662838b1036272aa1db19487673
Euler0525 <13804800525@139.com>
2022-09-09 11:01:02 +0800

    LICENSE&MANIFEST

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
+
+def readmeFile():
+    with open("README.rst", encoding="utf-8") as f:
+        return f.read()
+
+
 setup(name="lsb-steg",
       version="1.0.0",
       description="LSB Steganography for text",
-      packages=["lsb"], py_modules=["test"],
+      long_description=readmeFile(),
+      packages=["src"], py_modules=["test"],
+      license="MIT",
       author="Euler0525",
       author_email="13804800525@139.com")
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-import lsb
+from src import *
+
 
 def main():
     while True:
@@ -8,14 +9,14 @@ def main():
                 "请输入原始图片文件名(default=\"./test/encode_image.png\"):") or "./test/encode_image.png"
             encode_text = input(
                 "请输入待隐藏信息的存储位置(default=\"./test/encode_text.txt\"):") or "./test/encode_text.txt"
-            encode_obj = lsb.endetext.LSB(encode_image, encode_text)
+            encode_obj = endetext.LSB(encode_image, encode_text)
             encode_obj.lsbEncode()
         elif choice == "2":
             decode_image = input(
                 "请输入藏有信息的图片的文件名(default=\"./test/decode_image.png\"): ") or "./test/decode_image.png"
             decode_text = input(
                 "请输入信息的存储位置(default=\"./test/decode_text.txt\"): ") or "./test/decode_text.txt"
-            decode_obj = lsb.endetext.LSB(decode_image)
+            decode_obj = endetext.LSB(decode_image)
             decode_obj.lsbDecode(decode_text)
         elif choice == "3":
             exit()
